4caafc261d1d9a15d0319105c98e7789abc1f944
= <tranminhtri9090@gmail.com>
2020-07-26 10:42:17 +1000

    Adding styles to Components in MenuItem and homepage

--- src/components/menu-item/menu-item-component.js
+++ src/components/menu-item/menu-item-component.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './menu-item-component.scss';
 
 const MenuItem = ({ name, imageUrl, linkUrl, size }) => (
-    <div className={ `${size} menu-item` } style={{ backgroundImage: `url(${imageUrl})` }}>
-        <div className="content"> 
-            <h1 className="title">{name}</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
+    <div className={ `${size} menu-item` } >
+           <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}/>
+            <div className="content"> 
+                <h1 className="title">{name.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
     </div>
 );
 
