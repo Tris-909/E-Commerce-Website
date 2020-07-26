5b6956c72e4a7c4d7f7802ec41bb5127b681cfda
= <tranminhtri9090@gmail.com>
2020-07-26 13:37:28 +1000

    Adding navbar Component as header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import {Switch ,Route} from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
